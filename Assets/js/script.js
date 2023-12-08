86ef1c50c9b76b67ba19b4e23112ee9bd3bc0146
Derek Moy <derekymoy@gmail.com>
2023-12-07 17:59:02 -0800

    Initial commit. Added button to open modal.

---
+++ Assets/js/script.js
@@ -0,0 +1,9 @@
+var musicModal = document.getElementById("music-modal-card");
+
+function showModal() {
+    musicModal.classList.add('is-active');
+}
+
+function closeModal() {
+    musicModal.classList.remove('is-active');
+}
