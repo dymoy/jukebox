b8e038717d9263f646c35ab4cec4393897bd9c4c
Derek Moy <derekymoy@gmail.com>
2023-12-11 17:08:29 -0800

    Added TODO items into JavaScript file. Added children to Modal body element.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,3 +7,13 @@ function showModal() {
 function closeModal() {
     musicModal.classList.remove('is-active');
 }
+
+// TODO: getAPI() function uses fetch method to get song data 
+
+// TODO: getRandomIndex() function to get random song object from API fetch data 
+
+// TODO: Display song information into Modal HTML element 
+
+// TODO: Favorite functionality: store song into local storage with (key: song title, val: song object)
+
+// TODO: queryURL() function constructs URL to fetch from Web API 
